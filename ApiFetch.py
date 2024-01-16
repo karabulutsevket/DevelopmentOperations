afd26cad9ec456e168a5fc359f745a51059274b6
mirrolepth <sevketkarabulut@outlook.com.tr>
2024-01-16 20:50:15 +0300

    first commit

---
+++ ApiFetch.py
@@ -0,0 +1,16 @@
+import requests
+
+url = "https://api.n.exchange/en/api/v1/price/BTCUSD/latest/?market_code=nex"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    
+    data = response.json()
+    
+    bid_value = data[0]["ticker"]["bid"]
+
+    
+    print("1 BTC = ", bid_value)
+else:
+    print("Error:", response.status_code)
